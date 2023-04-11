49a0388434169a60bfdb8b03890f82ef40a5d34c
Randy Lien <randylien@gmail.com>
2023-04-11 21:02:52 +0800

    add wagami support

--- remix.config.js
+++ remix.config.js
@@ -4,4 +4,13 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverDependenciesToBundle: [
+    '@rainbow-me/rainbowkit',
+    '@rainbow-me/rainbowkit/wallets',
+    /^@?wagmi.*/,
+    /.*/,
+  ],
+  future: {
+    v2_routeConvention: true
+  }
 };
